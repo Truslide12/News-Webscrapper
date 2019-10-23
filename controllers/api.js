6d8baf7fa15a1a02b335478eb39c722afcf60a2f
Carlos Aragon <carlos.s.aragon12@gmail.com>
2019-10-22 18:32:39 -0700

    reformatted the file setup to make it clearer and got handlebars working with the data and added css formating, save and note buttons. also setup the saved articles page

---
+++ controllers/api.js
@@ -0,0 +1,188 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+const request = require("request"); //Makes http calls
+const cheerio = require("cheerio");
+
+// Routes
+
+// A GET route for scraping the echoJS website
+router.get("/scrape", function (req, res) {
+    console.log("scrape ran");
+    // First, we grab the body of the html with axios
+
+    axios.get("http://www.oann.com/", (error, response) => {
+        if (!error && resopnse.statusCode === 200) {
+            // Then, we load that into cheerio and save it to $ for a shorthand selector
+            var $ = cheerio.load(response.data);
+
+            // Now, we grab every h3 within an article tag, and do the following:
+            $("article h3").each(function (i, element) {
+                // Save an empty result object
+                let result = {};
+                let count = i;
+                // Add the text and href of every link, and save them as properties of the result object
+                result.title = $(this)
+                    .children("a")
+                    .text();
+                result.link = $(this)
+                    .children("a")
+                    .attr("href");
+                result.img = $(this)
+                    .children("a")
+                    .attr("img");
+                // Create a new Article using the `result` object built from scraping
+                db.Article.create(result)
+                    .then(function (dbArticle) {
+                        count ++;
+                        // View the added result in the console
+                        console.log(dbArticle);
+                    })
+                    .catch(function (err) {
+                        // If an error occurred, log it
+                        console.log(err);
+                    });
+            });
+            // render index
+            res.redirect('/');
+        }
+        else if (error || response.statusCode != 200) {
+            // Send a message to the client
+            res.send("Scrape Complete");
+        }
+    })
+});
+
+
+router.get("/", (req, res) => {
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            const retrievedArticles = dbArticle;
+            let hbsObject;
+            hbsObject = {
+                articles: dbArticle
+            };
+            res.render("index", hbsObject);        
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+router.get("/saved", (req, res) => {
+    db.Article.find({isSaved: true})
+        .then(function (retrievedArticles) {
+            // If we were able to successfully find Articles, send them back to the client
+            let hbsObject;
+            hbsObject = {
+                articles: retrievedArticles
+            };
+            res.render("saved", hbsObject);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for getting all Articles from the db
+router.get("/articles", function (req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+        .then(function (dbArticle) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Save article
+router.put("/save/:id", function (req, res) {
+    db.Article.findOneAndUpdate({ _id: req.params.id }, { isSaved: true })
+        .then(function (data) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(data);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });;
+});
+
+// Remove an Article from Saved
+router.put("/remove/:id", function (req, res) {
+    db.Article.findOneAndUpdate({ _id: req.params.id }, { isSaved: false })
+        .then(function (data) {
+            // If we were able to successfully find Articles, send them back to the client
+            res.json(data)
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+
+// Route for grabbing a specific Article by id, populate it with it's note
+router.get("/articles/:id", function (req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+        // ..and populate all of the notes associated with it
+        .populate({
+            path: "note",
+            model: "Note"
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully find an Article with the given id, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Route for saving/updating an Article's associated Note
+router.post("/note/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+        .then(function (dbNote) {
+            // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+            // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+            // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+            return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+// Delete a note
+router.delete("/note/:id", function (req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.findByIdAndRemove({ _id: req.params.id })
+        .then(function (dbNote) {
+
+            return db.Article.findOneAndUpdate({ note: req.params.id }, { $pullAll: [{ note: req.params.id }]});
+        })
+        .then(function (dbArticle) {
+            // If we were able to successfully update an Article, send it back to the client
+            res.json(dbArticle);
+        })
+        .catch(function (err) {
+            // If an error occurred, send it to the client
+            res.json(err);
+        });
+});
+
+module.exports = router;
